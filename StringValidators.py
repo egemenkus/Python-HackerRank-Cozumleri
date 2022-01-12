aeb5edeb792256a4b31bc025642e840fc6d81e00
Egemen Kuş <egemenutku.kus@ogr.deu.edu.tr>
2022-01-12 19:50:57 +0300

    Update StringValidators.py

--- StringValidators.py
+++ StringValidators.py
@@ -9,9 +9,9 @@
 # True
 
 if __name__ == '__main__':
-    giris = input()
-    print(any(c.isalnum() for c in giris))
-    print(any(c.isalpha() for c in giris))
-    print(any(c.isdigit() for c in giris))
-    print(any(c.islower() for c in giris))
-    print(any(c.isupper() for c in giris))
+    s = input()
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
